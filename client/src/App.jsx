1c62230be0f92760c1a08f5e0c9ace9c42c09f2d
Hammad Raheel Sarwar <hammadraheel.cs@gmail.com>
2026-08-03 17:47:19 +0600

    Add automatic path redirect for direct portal URLs and fix asset resolution

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import Meetings from './pages/Meetings';
 import MeetingDetails from './pages/MeetingDetails';
@@ -9,9 +10,20 @@ import TeacherDashboard from './pages/TeacherDashboard';
 import ParentDashboard from './pages/ParentDashboard';
 import AdminDashboard from './pages/AdminDashboard';
 
+function PathRedirect() {
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path && path !== '/' && path !== '/index.html' && !window.location.hash) {
+      window.location.replace('/#' + path);
+    }
+  }, []);
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <PathRedirect />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/meetings" element={<Meetings />} />
@@ -22,6 +34,7 @@ function App() {
         <Route path="/dashboard/teacher" element={<TeacherDashboard />} />
         <Route path="/dashboard/parent" element={<ParentDashboard />} />
         <Route path="/dashboard/admin" element={<AdminDashboard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
